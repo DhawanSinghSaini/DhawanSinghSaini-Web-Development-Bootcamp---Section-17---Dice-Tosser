4f136341b2ab27d0a42668431b03d4ef95a54bd1
DhawanSinghSaini <161929806+DhawanSinghSaini@users.noreply.github.com>
2024-09-12 15:03:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var img1 = Math.floor(Math.random()*6+1)
+var img2 = Math.floor(Math.random()*6+1)
+
+document.getElementsByClassName("img1")[0].setAttribute("src","./images/dice"+img1+".png") 
+document.getElementsByClassName("img2")[0].setAttribute("src","./images/dice"+img2+".png")
+
+if (img1>img2){
+    document.querySelector("h1").textContent="🥇 Player 1 Won!"
+}else if(img1<img2){
+    document.querySelector("h1").textContent="Player 2 Won! 🥇"
+}else if(img1==img2){
+    document.querySelector("h1").textContent="It's a Draw"
+}else{
+    document.querySelector("h1").textContent="Refresh Me"
+}
