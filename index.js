a444e61ded703b81d1d49a95f07b9ab8b5996fcc
DhawanSinghSaini <161929806+DhawanSinghSaini@users.noreply.github.com>
2024-09-12 15:05:48 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var img1 = Math.floor(Math.random()*6+1)
 var img2 = Math.floor(Math.random()*6+1)
 
-document.getElementsByClassName("img1")[0].setAttribute("src","./images/dice"+img1+".png") 
-document.getElementsByClassName("img2")[0].setAttribute("src","./images/dice"+img2+".png")
+document.getElementsByClassName("img1")[0].setAttribute("src","./dice"+img1+".png") 
+document.getElementsByClassName("img2")[0].setAttribute("src","./dice"+img2+".png")
 
 if (img1>img2){
     document.querySelector("h1").textContent="🥇 Player 1 Won!"
